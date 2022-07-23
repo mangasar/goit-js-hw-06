8ceb5d8b6737df653969acbc277bfdcfa029a45c
Mangasar <mangasarpetrosyan@gmail.com>
2022-07-23 10:57:27 +0300

    update

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,27 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+// const elems = ingredients.map( (ingredient) => {
+//   const elem = document.createElement("li");
+//   elem.innerText = ingredient;
+//   elem.classList.add("item");
+//   return elem;
+// });
+
+// const list = document.getElementById("ingredients");
+// list.append(...elems);
+
+
+
+const elems = ingredients.map( (ingredient) => { 
+  const elem = document.createElement("li")
+  elem.innerText = ingredient;
+  elem.classList.add("item");
+  return elem;
+
+});
+const list = document.getElementById("ingredients");
+list.append(...elems);
